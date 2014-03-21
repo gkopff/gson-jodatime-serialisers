b30a00022d23407e6ab2c6c98735d506e4e57de8
Greg <gkopff@highflyingobjects.com.au>
2014-03-21 13:28:03 +0800

    Use IntelliJ copyright template.

--- src/main/java/com/fatboyindustrial/gsonjodatime/package-info.java
+++ src/main/java/com/fatboyindustrial/gsonjodatime/package-info.java
@@ -1,4 +1,4 @@
-/* **********************************************************************************
+/*
  * GSON Joda Time Serialisers
  *
  * Copyright 2013-2014 Greg Kopff
@@ -21,8 +21,7 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
- *
- * ******************************************************************************* */
+ */
 
 /**
  *  Contains {@link com.google.gson.JsonSerializer} and {@link com.google.gson.JsonDeserializer} 
