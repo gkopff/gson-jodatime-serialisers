74be3842667bd116c13ab6ed3ca69244fb3c147e
Greg Kopff <gkopff@highflyingojects.com.au>
2017-03-29 08:45:30 +1000

    Align code style.

--- src/main/java/com/fatboyindustrial/gsonjodatime/InstantConverter.java
+++ src/main/java/com/fatboyindustrial/gsonjodatime/InstantConverter.java
@@ -32,7 +32,6 @@ import com.google.gson.JsonParseException;
 import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
-
 import org.joda.time.Instant;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
@@ -42,27 +41,53 @@ import java.lang.reflect.Type;
 /**
  * GSON serialiser/deserialiser for converting Joda {@link Instant} objects.
  */
-public class InstantConverter implements JsonSerializer<Instant>, JsonDeserializer<Instant> {
+public class InstantConverter implements JsonSerializer<Instant>, JsonDeserializer<Instant>
+{
+  /** Formatter. */
+  private static final DateTimeFormatter fmt = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").withZoneUTC();
 
-    private static final String PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
-    private static final DateTimeFormatter fmt = DateTimeFormat.forPattern(PATTERN).withZoneUTC();
+  /**
+   * Gson invokes this call-back method during serialization when it encounters a field of the
+   * specified type. <p>
+   *
+   * In the implementation of this call-back method, you should consider invoking
+   * {@link JsonSerializationContext#serialize(Object, Type)} method to create JsonElements for any
+   * non-trivial field of the {@code src} object. However, you should never invoke it on the
+   * {@code src} object itself since that will cause an infinite loop (Gson will call your
+   * call-back method again).
+   * @param src the object that needs to be converted to Json.
+   * @param typeOfSrc the actual type (fully genericized version) of the source object.
+   * @return a JsonElement corresponding to the specified object.
+   */
+  @Override
+  public JsonElement serialize(Instant src, Type typeOfSrc, JsonSerializationContext context)
+  {
+    return new JsonPrimitive(fmt.print(src));
+  }
 
-    @Override
-    public JsonElement serialize(Instant src, Type typeOfSrc, JsonSerializationContext context) {
-        String value = fmt.print(src);
-
-        return new JsonPrimitive(value);
+  /**
+   * Gson invokes this call-back method during deserialization when it encounters a field of the
+   * specified type. <p>
+   *
+   * In the implementation of this call-back method, you should consider invoking
+   * {@link JsonDeserializationContext#deserialize(JsonElement, Type)} method to create objects
+   * for any non-trivial field of the returned object. However, you should never invoke it on the
+   * the same type passing {@code json} since that will cause an infinite loop (Gson will call your
+   * call-back method again).
+   * @param json The Json data being deserialized
+   * @param typeOfT The type of the Object to deserialize to
+   * @return a deserialized object of the specified type typeOfT which is a subclass of {@code T}
+   * @throws JsonParseException if json is not in the expected format of {@code typeOfT}
+   */
+  @Override
+  public Instant deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+      throws JsonParseException
+  {
+    if (json.getAsString() == null || json.getAsString().isEmpty())
+    {
+      return null;
     }
 
-    @Override
-    public Instant deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
-            throws JsonParseException {
-
-        if (json.getAsString() == null || json.getAsString().isEmpty()) {
-            return null;
-        }
-
-        return Instant.parse(json.getAsString(), fmt);
-    }
-
+    return Instant.parse(json.getAsString(), fmt);
+  }
 }
